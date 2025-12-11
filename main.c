271af1c730bd19eecd39c36604706c7b3dd0edec
Gor-2001 <gormelqumyan6@gmail.com>
2025-12-11 18:48:23 +0400

    Finish the crush, second time.

--- main.c
+++ main.c
@@ -242,11 +242,11 @@ int main(int argc, char *argv[]) {
     printf("Summaries will be written to: %s\n\n", summary_filename);
     
     test_func(RNG_BAD, summary_filename);
-    //test_func(RNG_CHACHA20, summary_filename);
-    //test_func(RNG_CHACHA12, summary_filename);
-    //test_func(RNG_CHACHA8, summary_filename);
-    //test_func(RNG_CHACHA4, summary_filename);
-    //test_func(RNG_CHACHA4_FEEDBACK, summary_filename);
+    test_func(RNG_CHACHA20, summary_filename);
+    test_func(RNG_CHACHA12, summary_filename);
+    test_func(RNG_CHACHA8, summary_filename);
+    test_func(RNG_CHACHA4, summary_filename);
+    test_func(RNG_CHACHA4_FEEDBACK, summary_filename);
     
     printf("========================================\n");
     printf("All tests completed\n");
